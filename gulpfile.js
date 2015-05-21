3c41209ad9e5ed3c38db0be68ee19dedf64bd9a3
Martins Sipenko <martins.sipenko@accenture.com>
2015-05-21 09:03:34 +0000

    Compile icons before mixing less

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,10 @@ var elixir = require('laravel-elixir');
 require('laravel-elixir-icons');
 
 elixir(function(mix) {
-    mix.less('app.less')
-       .icons({
+    mix.icons({
            template: 'resources/assets/less/_icons.less',
        })
+       .less('app.less')
        .browserify('index.js')
        .copy('node_modules/bootstrap/fonts/', 'public/fonts/')
        .copy('node_modules/font-awesome/fonts/', 'public/fonts/')
