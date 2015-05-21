cff7592927df466afee3e44a09c8de4fed3a131b
Martins Sipenko <martins.sipenko@accenture.com>
2015-05-21 07:15:19 +0000

    Few sections complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var elixir = require('laravel-elixir');
+require('laravel-elixir-icons');
 
 elixir(function(mix) {
     mix.less('app.less')
+       .icons({
+           template: 'resources/assets/less/_icons.less',
+       })
        .browserify('index.js')
        .copy('node_modules/bootstrap/fonts/', 'public/fonts/')
        .copy('node_modules/font-awesome/fonts/', 'public/fonts/')
