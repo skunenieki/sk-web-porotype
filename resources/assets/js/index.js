525b6ed0dd62f0966cc1067edce5c23a2bce377b
Martins Sipenko <martins.sipenko@gmail.com>
2018-08-06 11:16:32 +0300

    Added 2017 galleries

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -177,12 +177,12 @@ $(window).load(function() {
                 'kind': 'album',
                 'alt': 'json',
                 'v': '2.0',
-                'max-results': '5',
+                'max-results': '2',
                 'fields': "entry(gphoto:id,title,media:group(media:content(@url))),title",
             }, function(json) {
                 var galleryBoxes = $('.gallery-box');
                 $.each(json.feed.entry, function(idx, obj) {
-                    var item = galleryBoxes[idx+3];
+                    var item = galleryBoxes[idx+6];
                     $(item).find('.project-category').html(obj.title.$t);
                     $(item).attr('href', 'https://plus.google.com/u/0/photos/' + json.feed.title.$t + '/albums/' + obj.gphoto$id.$t);
                     $(item).find('img').attr('src', obj.media$group.media$content[0].url.replace(/\/(?=[^\/]*$)/, '/s512-c/'));
