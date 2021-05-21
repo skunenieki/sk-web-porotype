3252c7ffd7dc88e753e45f17d973a36690977848
Martins Sipenko <martins.sipenko@gmail.com>
2021-05-21 11:56:48 +0300

    remove all content

--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -1,10 +1,10 @@
-require('jquery-lazyload/jquery.lazyload');
+// require('jquery-lazyload/jquery.lazyload');
 
-$(function() {
-    $("img.lazy").lazyload({
-        event : "loadSponsorImages"
-    });
-});
+// $(function() {
+//     $("img.lazy").lazyload({
+//         event : "loadSponsorImages"
+//     });
+// });
 
 require('bootstrap/js/modal');
 require('bootstrap/js/modal');
@@ -14,10 +14,10 @@ require('moment/locale/lv');
 moment.locale('lv');
 
 require('retina.js');
-require('zepto.backstretch/zepto.backstretch.min');
+// require('zepto.backstretch/zepto.backstretch.min');
 
-require('twitter-fetcher');
-require('./timecircles');
+// require('twitter-fetcher');
+// require('./timecircles');
 
 var countdown =  $('#countdown');
 
@@ -134,66 +134,66 @@ window.loadResults = function (id) {
 }
 
 $(window).on('load', function() {
-    twitterFetcher.fetch({
-        // id: '579257548878061568',
-        profile: {
-            screenName: 'skunenieki',
-        },
-        domId: 'timeline',
-        maxTweets: 5,
-        enableLinks: true,
-        showUser: true,
-        showImages: false,
-        showInteraction: false,
-        customCallback: function(tweets) {
-            var n = 0;
-            var x = tweets.length;
-            var element = document.getElementById('timeline');
-            var html = '';
-            var el = document.createElement('div');
-            while (n < x) {
-                el.innerHTML = tweets[n];
+    // twitterFetcher.fetch({
+    //     // id: '579257548878061568',
+    //     profile: {
+    //         screenName: 'skunenieki',
+    //     },
+    //     domId: 'timeline',
+    //     maxTweets: 5,
+    //     enableLinks: true,
+    //     showUser: true,
+    //     showImages: false,
+    //     showInteraction: false,
+    //     customCallback: function(tweets) {
+    //         var n = 0;
+    //         var x = tweets.length;
+    //         var element = document.getElementById('timeline');
+    //         var html = '';
+    //         var el = document.createElement('div');
+    //         while (n < x) {
+    //             el.innerHTML = tweets[n];
 
-                console.log(el.getElementsByClassName('timePosted')[0].innerHTML);
+    //             console.log(el.getElementsByClassName('timePosted')[0].innerHTML);
 
-                html += '<li class="' + (n%2 === 0 ? 'pos-right' : 'pos-left') + ' clearfix">';
-                html += '<div class="timeline-badge"><i class="fa fa-twitter"></i></div>';
-                html += '<div class="message"><div class="media">';
-                html += '<div class="media-left">';
-                html += '<a href="' + el.getElementsByClassName('user')[0].getElementsByTagName('a')[0].href + '">';
-                html += '<img class="media-object" src="' + el.getElementsByClassName('user')[0].getElementsByTagName('a')[0].getElementsByTagName('img')[0].src.replace('normal', '200x200') + '"/>';
-                html += '</a>';
-                html += '</div>';
-                html += '<div class="media-body">';
-                html += '<h4 class="media-heading">';
-                html += '<a href="' + el.getElementsByClassName('user')[0].getElementsByTagName('a')[0].href + '">';
-                html += el.getElementsByClassName('user')[0].getElementsByTagName('div')[0].getElementsByTagName('a')[0].getElementsByTagName('span')[0].getElementsByTagName('span')[0].innerHTML;
-                html += '</a>';
-                html += '</h4>';
-                html += '<div class="timline-time"><small class="text-muted"><i class="fa fa-clock-o"></i> ';
-                html += el.getElementsByClassName('timePosted')[0].innerHTML;
-                html += '</small></div>';
-                html += '<p>' + el.getElementsByClassName('tweet')[0].innerHTML + '</p>';
-                html += '</div>';
-                html += '</div>';
-                html += '</div>';
-                html += '</li>';
+    //             html += '<li class="' + (n%2 === 0 ? 'pos-right' : 'pos-left') + ' clearfix">';
+    //             html += '<div class="timeline-badge"><i class="fa fa-twitter"></i></div>';
+    //             html += '<div class="message"><div class="media">';
+    //             html += '<div class="media-left">';
+    //             html += '<a href="' + el.getElementsByClassName('user')[0].getElementsByTagName('a')[0].href + '">';
+    //             html += '<img class="media-object" src="' + el.getElementsByClassName('user')[0].getElementsByTagName('a')[0].getElementsByTagName('img')[0].src.replace('normal', '200x200') + '"/>';
+    //             html += '</a>';
+    //             html += '</div>';
+    //             html += '<div class="media-body">';
+    //             html += '<h4 class="media-heading">';
+    //             html += '<a href="' + el.getElementsByClassName('user')[0].getElementsByTagName('a')[0].href + '">';
+    //             html += el.getElementsByClassName('user')[0].getElementsByTagName('div')[0].getElementsByTagName('a')[0].getElementsByTagName('span')[0].getElementsByTagName('span')[0].innerHTML;
+    //             html += '</a>';
+    //             html += '</h4>';
+    //             html += '<div class="timline-time"><small class="text-muted"><i class="fa fa-clock-o"></i> ';
+    //             html += el.getElementsByClassName('timePosted')[0].innerHTML;
+    //             html += '</small></div>';
+    //             html += '<p>' + el.getElementsByClassName('tweet')[0].innerHTML + '</p>';
+    //             html += '</div>';
+    //             html += '</div>';
+    //             html += '</div>';
+    //             html += '</li>';
 
-                n++;
-            }
-            element.innerHTML = html;
-        },
-        dateFunction: function(newDate, datetimeText) {
-            return moment(newDate).add(2, 'hour').tz('Europe/Riga').format('LLL');
-        }
-    });
+    //             n++;
+    //         }
+    //         element.innerHTML = html;
+    //     },
+    //     dateFunction: function(newDate, datetimeText) {
+    //         return moment(newDate).add(2, 'hour').tz('Europe/Riga').format('LLL');
+    //     }
+    // });
 
-    $.backstretch([
-        '/img/bg2.jpg',
-        '/img/bg3.jpg',
-        '/img/bg4.jpg',
-        '/img/bg5.jpg',
-      ], {duration: 4000, fade: 750});
+    // $.backstretch([
+    //     '/img/bg2.jpg',
+    //     '/img/bg3.jpg',
+    //     '/img/bg4.jpg',
+    //     '/img/bg5.jpg',
+    //   ], {duration: 4000, fade: 750});
 
     $('#preloader .spinner').hide();
     $('#preloader').delay(350).fadeOut('slow');
@@ -253,57 +253,57 @@ $(window).on('load', function() {
         });
     // }
 
-    if (moment().diff('2020-08-08T11:00:00.000+0300', 'seconds') < 0) {
-        createTimeCicles();
-        countdown.delay(350).addClass('animated bounceIn');
+    // if (moment().diff('2020-08-08T11:00:00.000+0300', 'seconds') < 0) {
+    //     createTimeCicles();
+    //     countdown.delay(350).addClass('animated bounceIn');
 
-        $(window).on('resize', function() {
-            countdown.TimeCircles().destroy();
-            createTimeCicles();
-        });
-    }
+    //     $(window).on('resize', function() {
+    //         countdown.TimeCircles().destroy();
+    //         createTimeCicles();
+    //     });
+    // }
 
-    $('img.lazy').trigger('loadSponsorImages');
+    // $('img.lazy').trigger('loadSponsorImages');
 });
 
 jQuery('#q').on('click', function() {
     ga('send', 'event', 'search', 'click');
 });
 
-jQuery('.gallery-box').on('click', function() {
-    ga('send', 'event', 'gallery', 'click');
-});
+// jQuery('.gallery-box').on('click', function() {
+//     ga('send', 'event', 'gallery', 'click');
+// });
 
-jQuery('#nolikums').on('click', function() {
-    ga('send', 'event', 'nolikums', 'click');
-});
+// jQuery('#nolikums').on('click', function() {
+//     ga('send', 'event', 'nolikums', 'click');
+// });
 
-$(function () {
-    var parent = $(".sponsors .shuffle");
-    var sponsorLogos = parent.children();
-    while (sponsorLogos.length) {
-        parent.append(sponsorLogos.splice(Math.floor(Math.random() * sponsorLogos.length), 1)[0]);
-    }
-});
+// $(function () {
+//     var parent = $(".sponsors .shuffle");
+//     var sponsorLogos = parent.children();
+//     while (sponsorLogos.length) {
+//         parent.append(sponsorLogos.splice(Math.floor(Math.random() * sponsorLogos.length), 1)[0]);
+//     }
+// });
 
-function createTimeCicles() {
-    countdown.TimeCircles({
-        fg_width: 0.013,
-        bg_width: 0.6,
-        circle_bg_color: '#ffffff',
-        text_size: 0.09,
-        time: {
-            Days: {color: '#ccc'},
-            Hours: {color: '#ccc'},
-            Minutes: {color: '#ccc'},
-            Seconds: {color: '#ccc'},
-        }
-    });
-    $('.textDiv_Days h4').html('Dienas');
-    $('.textDiv_Hours h4').html('Stundas');
-    $('.textDiv_Minutes h4').html('Minūtes');
-    $('.textDiv_Seconds h4').html('Sekundes');
-}
+// function createTimeCicles() {
+//     countdown.TimeCircles({
+//         fg_width: 0.013,
+//         bg_width: 0.6,
+//         circle_bg_color: '#ffffff',
+//         text_size: 0.09,
+//         time: {
+//             Days: {color: '#ccc'},
+//             Hours: {color: '#ccc'},
+//             Minutes: {color: '#ccc'},
+//             Seconds: {color: '#ccc'},
+//         }
+//     });
+//     $('.textDiv_Days h4').html('Dienas');
+//     $('.textDiv_Hours h4').html('Stundas');
+//     $('.textDiv_Minutes h4').html('Minūtes');
+//     $('.textDiv_Seconds h4').html('Sekundes');
+// }
 
 function debounce(func, wait, immediate) {
     var timeout;
